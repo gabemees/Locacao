ea0bd90c192e5530d33206cd58e0e3946f313eec
Lucas Gustavo <lucas.gustavo.santos@hotmail.com>
2016-06-13 21:59:15 -0300

    Adicionando Entidades

---
+++ Grupo.Locacao.Dominio.Teste/DominioTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grupo.Locacao.Dominio.Entidades;
+
+namespace Grupo.Locacao.Dominio.Teste
+{
+    [TestClass]
+    public class DominioTest
+    {
+        [TestMethod]
+        public void CriarLocacaoTeste()
+        {
+            LocacaoCarro locacao = new LocacaoCarro(new DateTime(2016, 06, 13), true, new Cliente("Lucas", "10672228980", "99188736"), new Carro("Gol", "VW", "MGT-8899"));
+        }
+    }
+}
--- Grupo.Locacao.Dominio/Entidades/Carro.cs
+++ Grupo.Locacao.Dominio/Entidades/Carro.cs
@@ -8,5 +8,24 @@ namespace Grupo.Locacao.Dominio.Entidades
 {
     public class Carro
     {
+        public Carro()
+        {
+        }
+
+        public Carro(string nome, string marca, string placa)
+        {
+            Nome = nome;
+            Marca = marca;
+            Placa = placa;
+        }
+
+        public string Marca { get; set; }
+        public string Nome { get; set; }
+        public string Placa { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} - {2}", Nome, Marca, Placa);
+        }
     }
 }
