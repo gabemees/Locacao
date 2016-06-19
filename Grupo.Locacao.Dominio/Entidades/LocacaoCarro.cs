cdb9307c6e66b5752a4414183350b81d47172cd6
Lucas Gustavo <lucas.gustavo.santos@hotmail.com>
2016-06-19 18:08:57 -0300

    LocacaoCarro

---
+++ Grupo.Locacao.Dominio/Entidades/LocacaoCarro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo.Locacao.Dominio.Entidades
+{
+    public class LocacaoCarro
+    {
+        public LocacaoCarro()
+        {
+        }
+
+        public LocacaoCarro(DateTime data, bool status, Cliente cliente, Carro carro)
+        {
+            DataLocacao = data;
+            Status = status;
+            Clientes = cliente;
+            Carros = carro;
+        }
+
+        public Carro Carros { get; set; }
+        public Cliente Clientes { get; set; }
+        public DateTime DataLocacao { get; set; }
+        public bool Status { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} | {1} | {2} | {3}", Clientes, Carros, DataLocacao, Status== true ? "Ocupado": "Disponivel");
+        }
+    }
+}
